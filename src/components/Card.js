553609fe388ce8d199c58e5dc593c7e629dd750b
Andres Mora Vanegas <andymora1907@hotmail.com>
2019-07-09 03:58:38 -0500

    [update]second commit ui working

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import like from '../assets/img/like.svg';
 
-function Card() {
+function Card(data) {
+
+    let [count, setCount] = useState(0);
+    let [vote, setVote] = useState(0);
+
+
+    function handleVote(e, user) {
+        e.preventDefault();
+        let actualData = JSON.parse(localStorage.getItem('actualData'));
+        console.log('actualData', actualData);
+        const idx = actualData.findIndex(r => r.id == user.id);
+        debugger
+        if (idx > -1) {
+            if (vote == 1) {
+                actualData[idx].likes = actualData[idx].likes + 1;
+            }
+            if (vote == 2) {
+                actualData[idx].dislikes = actualData[idx].dislikes + 1
+            }
+        }
+        console.log('actualData', actualData);
+        console.log('m', idx);
+        console.log('e', e);
+        console.log('user', user);
+
+        setTimeout(() => {
+            setCount(count = 0);
+        }, 3000);
+    }
+
+    function assignVote(e) {
+        e.event.preventDefault()
+        setVote(vote = e.vote);
+        console.log('e', e);
+
+    }
+
+    const styl = {
+        backgroundImage: `url(../assets/img/${data.props.image}.jpg)`
+    }
     return (
-        <div className="app-card">
+        <div className={data.props.image + ' app-card'}>
             <div className="name">
                 <div className="vote">
                     <a href="#" className="like">
-                        <img src={like} />
+                        <img src={like} alt="like" />
                     </a>
                 </div>
                 <div className="label">
-                    <h2>Kaney West</h2>
+                    <h2>{data.props.name}</h2>
                 </div>
             </div>
             <div className="caption">
                 <div className="date">
-                    1 Month ago in Entertaiment
+                    <span>1 Month ago</span> in {data.props.topic}
                 </div>
                 <div className="detail">
                     <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eius temporibus delectus
-                        </p>
-                    <a href="#" className="like">
+
+                        {count === 1 || count === 2 ?
+                            <span>Thank you for your voting</span>
+                            :
+                            <span>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eius temporibus delectus
+                        </span>
+                        }
+
+                    </p>
+                    <a href="#" className={`like ${vote == 1 ? 'border' : ''}`} onClick={(e) => assignVote({ event: e, vote: 1, user: data.props.id })}>
                         <img src={like} />
                     </a>
-                    <a href="#" className="dislike">
+                    <a href="#" className={`dislike ${vote == 2 ? 'border' : ''}`} onClick={(e) => assignVote({ event: e, vote: 2, user: data.props.id })}>
                         <img src={like} />
                     </a>
-                    <a href="#" className="vote-btn">Vote now</a>
+                    <a href="#" className="vote-btn" onClick={(e) => handleVote(e, data.props)}>Vote now</a>
                 </div>
 
             </div>
-            <div className="votes"></div>
-            <div className="percent"></div>
+            <div className="percent">
+                <div className="rate-like" style={{ width: data.props.likes + '%' }}>
+                    <img src={like} alt="like" /><span>{data.props.likes + '%'}</span>
+                </div>
+                <div className="rate-dislike" style={{ width: data.props.dislikes + '%' }}>
+                    <img src={like} alt="dislike" /><span>{data.props.dislikes + '%'}</span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import pope2 from '../assets/img/pope2.png';
+import w from '../assets/img/wiki.png';
+import like from '../assets/img/like.svg';
 import '../App.scss';
 import Header from './Header';
 
@@ -11,14 +12,28 @@ function Hero() {
                 <Header />
             </div>
             <div className="two">
-
-            </div>
-            <footer className="four">
-                <div>
-                    <div>CLOSING IN</div>
+                <div className="vote-pope">
+                    <div className="descript">
+                        What´s your opinion on
+                        <h2>Pope Francis?</h2>
+                        <p>He's talking tough on clergy sexual abuse, but is he just another papal pervert protector? (thumbs down) or a true pedophile punishing pontiff? (thumbs up)</p>
+                        <a href="#"><img src={w} alt="wikipedia" /> More information </a>
+                        <h3>What's Your Verdict?</h3>
+                    </div>
+                    <div className="vote">
+                        <div className="like w-50">
+                            <img src={like} alt="wikipedia" />
+                        </div>
+                        <div className="dislike w-50">
+                            <img src={like} alt="wikipedia" />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div><span>22</span> Days</div>
+            </div>
+            <footer className="four align-end">
+                <div className="d-flex">
+                    <div className="w-50 closing tri">CLOSING IN</div>
+                    <div className="w-50 days"><span>22</span> Days</div>
                 </div>
             </footer>
 
