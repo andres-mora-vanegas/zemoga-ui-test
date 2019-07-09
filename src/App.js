f42897953b661c76a9081a2001cf52145591a39e
andres Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2019-07-09 11:27:42 -0500

    [update]finished exercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import Header from './components/Header'
 import Hero from './components/Hero'
 import Container from './components/Container'
 import Footer from './components/Footer'
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,95 +1,117 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import like from '../assets/img/like.svg';
 
-function Card(data) {
-
-    let [count, setCount] = useState(0);
-    let [vote, setVote] = useState(0);
+class Card extends Component {
 
 
-    function handleVote(e, user) {
-        e.preventDefault();
-        let actualData = JSON.parse(localStorage.getItem('actualData'));
-        console.log('actualData', actualData);
-        const idx = actualData.findIndex(r => r.id == user.id);
-        debugger
-        if (idx > -1) {
-            if (vote == 1) {
-                actualData[idx].likes = actualData[idx].likes + 1;
-            }
-            if (vote == 2) {
-                actualData[idx].dislikes = actualData[idx].dislikes + 1
-            }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            likes: 0,
+            dislikes: 0,
+            count: 0,
+            vote: 0,
+            actualData: [],
+            message: false
         }
-        console.log('actualData', actualData);
-        console.log('m', idx);
-        console.log('e', e);
-        console.log('user', user);
-
-        setTimeout(() => {
-            setCount(count = 0);
-        }, 3000);
     }
 
-    function assignVote(e) {
+    componentDidMount() {
+        let actualData = JSON.parse(localStorage.getItem('actualData'))
+        this.setState({
+            actualData: actualData
+        });
+        const i = actualData.findIndex(r => r.id == this.props.props.id);
+        this.setState({
+            likes: actualData[i].likes,
+            dislikes: actualData[i].dislikes,
+        });
+    }
+
+    handleVote(e, user) {
+        e.preventDefault();
+        const idx = this.state.actualData.findIndex(r => r.id == user.id);
+        const vote = this.state.vote;
+        if (idx > -1 && vote != 0) {
+            this.setState({
+                message: true,
+                vote: 0
+            });
+            if (vote === 1) {
+                this.state.actualData[idx].likes = this.state.actualData[idx].likes + 1;
+            }
+            if (vote === 2) {
+                this.state.actualData[idx].dislikes = this.state.actualData[idx].dislikes + 1
+            }
+            this.setState({
+                likes: this.state.actualData[idx].likes,
+                dislikes: this.state.actualData[idx].dislikes,
+            });
+            localStorage.setItem('actualData', JSON.stringify(this.state.actualData));
+            setTimeout(() => this.setState({ message: false }), 3000);
+        }
+    }
+
+    assignVote(e) {
         e.event.preventDefault()
-        setVote(vote = e.vote);
-        console.log('e', e);
-
+        this.setState({
+            vote: e.vote
+        });
     }
 
-    const styl = {
-        backgroundImage: `url(../assets/img/${data.props.image}.jpg)`
-    }
-    return (
-        <div className={data.props.image + ' app-card'}>
-            <div className="name">
-                <div className="vote">
-                    <a href="#" className="like">
-                        <img src={like} alt="like" />
-                    </a>
+    render() {
+        const styl = {
+            backgroundImage: `url(../assets/img/${this.props.image}.jpg)`
+        }
+        return (
+            <div className={this.props.props.image + ' app-card'}>
+                <div className="name">
+                    <div className="vote">
+                        <a href="#" className="like">
+                            <img src={like} alt="like" />
+                        </a>
+                    </div>
+                    <div className="label">
+                        <h2>{this.props.name}</h2>
+                    </div>
                 </div>
-                <div className="label">
-                    <h2>{data.props.name}</h2>
-                </div>
-            </div>
-            <div className="caption">
-                <div className="date">
-                    <span>1 Month ago</span> in {data.props.topic}
-                </div>
-                <div className="detail">
-                    <p>
-
-                        {count === 1 || count === 2 ?
-                            <span>Thank you for your voting</span>
-                            :
-                            <span>
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eius temporibus delectus
+                <div className="caption">
+                    <div className="date">
+                        <span>1 Month ago</span> in {this.props.topic}
+                    </div>
+                    <div className="detail">
+                        <p>
+                            {this.state.message ?
+                                <span>Thank you for your voting!</span>
+                                :
+                                <span>
+                                    Lorem ipsum, dolor sit amet
                         </span>
-                        }
+                            }
+                        </p>
+                        <a href="#" className={`like ${this.state.vote == 1 ? 'border' : ''}`} onClick={(e) => this.assignVote({ event: e, vote: 1, user: this.props.props.id })}>
+                            <img src={like} />
+                        </a>
+                        <a href="#" className={`dislike ${this.state.vote == 2 ? 'border' : ''}`} onClick={(e) => this.assignVote({ event: e, vote: 2, user: this.props.props.id })}>
+                            <img src={like} />
+                        </a>
+                        <a href="#" className="vote-btn" onClick={(e) => this.handleVote(e, this.props.props)}>Vote now</a>
+                    </div>
 
-                    </p>
-                    <a href="#" className={`like ${vote == 1 ? 'border' : ''}`} onClick={(e) => assignVote({ event: e, vote: 1, user: data.props.id })}>
-                        <img src={like} />
-                    </a>
-                    <a href="#" className={`dislike ${vote == 2 ? 'border' : ''}`} onClick={(e) => assignVote({ event: e, vote: 2, user: data.props.id })}>
-                        <img src={like} />
-                    </a>
-                    <a href="#" className="vote-btn" onClick={(e) => handleVote(e, data.props)}>Vote now</a>
                 </div>
-
-            </div>
-            <div className="percent">
-                <div className="rate-like" style={{ width: data.props.likes + '%' }}>
-                    <img src={like} alt="like" /><span>{data.props.likes + '%'}</span>
-                </div>
-                <div className="rate-dislike" style={{ width: data.props.dislikes + '%' }}>
-                    <img src={like} alt="dislike" /><span>{data.props.dislikes + '%'}</span>
+                <div className="percent">
+                    <div className="rate-like" style={{ width: this.state.likes + '%' }}>
+                        <img src={like} alt="like" /><span>{this.state.likes + '%'}</span>
+                    </div>
+                    <div className="rate-dislike" style={{ width: this.state.dislikes + '%' }}>
+                        <img src={like} alt="dislike" /><span>{this.state.dislikes + '%'}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Card;
