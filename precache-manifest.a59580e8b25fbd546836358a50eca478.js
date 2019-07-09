72f8bab715036b0dfe798aeee86e71d04049f2c9
andres Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2019-07-09 11:58:29 -0500

    [deploy]upload live version

---
+++ precache-manifest.a59580e8b25fbd546836358a50eca478.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c257990f4e3708d20ad113f8704d8ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e67cd77c453e24d2a0",
+    "url": "/static/css/main.53f9e73b.chunk.css"
+  },
+  {
+    "revision": "786c573acd440e7029ec",
+    "url": "/static/js/2.4a1dbf5a.chunk.js"
+  },
+  {
+    "revision": "a8e67cd77c453e24d2a0",
+    "url": "/static/js/main.e69482da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b09638665bb35da67fb87f3852649df",
+    "url": "/static/media/background-footer.0b096386.svg"
+  },
+  {
+    "revision": "2e72ac89aefb84c31338fa3b2737100a",
+    "url": "/static/media/cristina.2e72ac89.png"
+  },
+  {
+    "revision": "9192b93708181df60f3dfc2dbd2dbbfa",
+    "url": "/static/media/kaney.9192b937.png"
+  },
+  {
+    "revision": "5cd9defd56edbc8be4fe07c664e0d29d",
+    "url": "/static/media/like.5cd9defd.svg"
+  },
+  {
+    "revision": "16a48d1b9df13b9e3e385504fb091480",
+    "url": "/static/media/malala.16a48d1b.png"
+  },
+  {
+    "revision": "25c1dc3cc6a97750e10eedb3946c351b",
+    "url": "/static/media/mark.25c1dc3c.png"
+  },
+  {
+    "revision": "f669a2e71f77503718f3aee228326a74",
+    "url": "/static/media/people.f669a2e7.jpg"
+  },
+  {
+    "revision": "704e1733249cd2187fd46c8053129ce6",
+    "url": "/static/media/pope1.704e1733.png"
+  }
+]);
